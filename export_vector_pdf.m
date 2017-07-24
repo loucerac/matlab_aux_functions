fd20a184b6a9cb36abde685249e4100f75693e92
clouceram <carlos.loucera@alumnos.unican.es>
2017-07-24 18:06:18 +0200

    Use already declared variables
    Improve code legibility by using already declared local variables.

--- export_vector_pdf.m
+++ export_vector_pdf.m
@@ -4,20 +4,20 @@ function [h, status, cmd] = export_vector_pdf(file_name, h)
 % build file_names
 svg_out = sprintf('%s.svg', file_name);
 pdf_out = sprintf('%s.pdf', file_name);
-    
+
 try
     % inkscape command line
     exe = '"C:\Program Files\Inkscape\inkscape.exe"';
     args = sprintf('%s --export-area-drawing --export-pdf=%s', svg_out, pdf_out);
-    
+
     % save as .svg (fully vectorized, supports tranparency)
     saveas(gcf,svg_out)
-    
+
     % execute inkscape
     system([exe ' ' args]);
-    
+
     % delete temporary files
-    delete([file_name '.svg'])
+    delete(svg_out)
 catch
     % Pure MATLAB (extremely big file size)
     set(h,'Units','Inches');
