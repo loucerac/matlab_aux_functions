2c7bde5e20a1573625025ecbb6d7a2abcf1f86ef
clouceram <carlos.loucera@alumnos.unican.es>
2017-07-24 18:54:59 +0200

    Code legibility fixes.
    Minor legibility fixes.

--- export_vector_pdf.m
+++ export_vector_pdf.m
@@ -1,6 +1,9 @@
 function [h, status, cmd] = export_vector_pdf(file_name, h)
 %EXPORT_VECTOR_PDF Create a fully vectorized PDF
 
+% NOTE: requires a functional x64 Inkscape installation in order to build
+%   a PDf with transparencies.
+
 % INPUT:
 % file_name: string: output base name
 % h: figure handle: figure to be exported
@@ -10,30 +13,37 @@ function [h, status, cmd] = export_vector_pdf(file_name, h)
 % status: int: OS CLI status code
 % cmd: string: OS system CLI command output
 
+% Carlos Loucera, University of Cantabria, 2017
+
 % build file_names
 svg_out = sprintf('%s.svg', file_name);
 pdf_out = sprintf('%s.pdf', file_name);
 
 try
+    %TODO: read the path from an external configuration file
     % inkscape command line
     exe = '"C:\Program Files\Inkscape\inkscape.exe"';
     args = sprintf('%s --export-area-drawing --export-pdf=%s', svg_out, pdf_out);
 
     % save as .svg (fully vectorized, supports tranparency)
-    saveas(gcf,svg_out)
+    saveas(h, svg_out)
 
     % execute inkscape
-    system([exe ' ' args]);
+    cli_str = sprintf('%s %s', exe, args);
+    system(cli_str);
 
     % delete temporary files
     delete(svg_out)
 catch
-    % Pure MATLAB (extremely big file size)
+    % Pure MATLAB (extremely big file size for 3D and image-like figures)
+    % No transparency allowed
     set(h,'Units','Inches');
     pos = get(h,'Position');
-    set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+    set(h,...
+        'PaperPositionMode', 'Auto', 'PaperUnits', 'Inches',...
+        'PaperSize',[pos(3), pos(4)])
     set(h, 'renderer', 'painter')
-    print(h,pdf_out,'-dpdf','-r0')
+    print(h, pdf_out,'-dpdf','-r0')
 end
 
 % crop pdf
